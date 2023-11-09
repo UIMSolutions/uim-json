24d3df0b7cf3a52c5b13c89ed9ca4e7af6fe6e4b
UIMSolutions <ui.manufaktur@gmail.com>
2023-11-09 12:45:37 +0100

    Update

--- source/uim/json/parser/package.d
+++ source/uim/json/parser/package.d
@@ -582,7 +582,9 @@
 //		// Default doesn't work well since store is a union.  Compare only
 //		// what should be in store.
 //		// This is @trusted to remain nogc, nothrow, fast, and usable from @safe code.
-//		if (type_tag != rhs.type_tag) { return false; }
+//		if (type_tag != rhs.type_tag) { 
+      return false; 
+    }
 //		
 //		final switch (type_tag)
 //		{
@@ -739,7 +741,9 @@
 //		auto c2 = peekChar();
 //		static if (!CaseSensitive) c2 = toLower(c2);
 //		
-//		if (c2 != c) { return false; }
+//		if (c2 != c) { 
+      return false; 
+    }
 //		
 //		getChar();
 //		return true;
